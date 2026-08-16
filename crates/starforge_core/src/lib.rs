6469e562d872ef14ac994c04397e0b8cfc40c38c
wangzekai <wangzekai.parallel@outlook.com>
2026-08-16 23:08:54 +0800

    Add core extractor

--- crates/starforge_core/src/lib.rs
+++ crates/starforge_core/src/lib.rs
@@ -2,8 +2,12 @@ pub mod archetype;
 pub mod bit_signature;
 pub mod component;
 pub mod entity;
+pub mod extract;
+pub mod system;
 pub mod types;
 
 pub mod macros {
     pub use starforge_core_macro::*;
 }
+
+pub struct Core {}
